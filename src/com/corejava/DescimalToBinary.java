5b3223ddbe28109f1b626524f67dc226ef3acb5b
akmaurya <akmaurya@yapstone.com>
2020-02-12 20:08:38 +0530

    Add BinaryDecimal Program

---
+++ src/com/corejava/DescimalToBinary.java
@@ -0,0 +1,38 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class DescimalToBinary {
+
+    public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int number;
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+        int nums = number;
+        // Actually this condition would be taken care by StringBuffer initialization.
+        if(nums == 0)
+            System.out.println(nums);
+        StringBuffer sb = new StringBuffer();
+        // Iterate by converting num to appropriate binary value.
+        while(nums!=0)
+        {
+            sb.append(nums%2);
+            nums = nums/2;
+        }
+        System.out.println(sb.toString());
+        //if(nums!=0)
+        System.out.println(sb.reverse().toString());
+
+
+        //////  Or  Next Way to print
+        System.out.println(Integer.toBinaryString(number));
+    }
+
+
+
+
+
+
+}
+
